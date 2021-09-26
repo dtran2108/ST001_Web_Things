a1a5b57a3ee4ef50d1f938dff03192238cb3f7e1
Tran Duong <dtran2108@gmail.com>
2021-09-27 06:35:04 +0700

    done product detail page

--- static/js/pro-detail.js
+++ static/js/pro-detail.js
@@ -17,3 +17,27 @@ plus.addEventListener('click', function(){
     }
     inputQuantity.value = value;
 })
+
+$(".product-carousel-item-container")
+  // tile mouse actions
+  .on("click", function() {
+    $(this)
+      .children(".product-carousel-item")
+      .css({ transform: "scale(" + $(this).attr("data-scale") + ")" });
+  })
+  .on("mouseout", function() {
+    $(this)
+      .children(".product-carousel-item")
+      .css({ transform: "scale(1)" });
+  })
+  .on("mousemove", function(e) {
+    $(this)
+      .children(".product-carousel-item")
+      .css({
+        "transform-origin":
+          ((e.pageX - $(this).offset().left) / $(this).width()) * 100 +
+          "% " +
+          ((e.pageY - $(this).offset().top) / $(this).height()) * 100 +
+          "%"
+      });
+  });
